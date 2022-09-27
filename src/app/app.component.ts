ab5c6e05c0bb596d3235ba04c97443a916268936
akyarbrough <ayarbrough0816@gmail.com>
2022-09-26 19:59:02 -0500

    Exercise 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'property-binding-image-size';
+  isDisabled:boolean=true;
+
+  toggleDisable(){
+    this.isDisabled = !this.isDisabled
+  }
 }
