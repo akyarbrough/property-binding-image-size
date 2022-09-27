aafd07c703c0457994244277b2a987e5d20cd233
akyarbrough <ayarbrough0816@gmail.com>
2022-09-26 20:12:21 -0500

    Exercise 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  
+
 }
